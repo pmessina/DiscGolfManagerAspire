5d834b11fcdbc089786e583a19934e408df8819f
pmessina <pmm0501@gmail.com>
2024-11-08 17:40:38 -0500

    Add Radzen library; Add grid for showing existing discs

--- DiscGolfManagerAspire.Web/Program.cs
+++ DiscGolfManagerAspire.Web/Program.cs
@@ -1,6 +1,7 @@
 using DiscGolfManagerAspire.Web;
 using DiscGolfManagerAspire.Web.Components;
 using DiscGolfManagerAspire.Web.Services;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddOutputCache();
 
